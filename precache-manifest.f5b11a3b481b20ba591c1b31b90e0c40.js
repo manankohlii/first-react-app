345e7b5a8db45577a6d4f7eeae813207a7cec1e4
Manan Kohli <mankoh00@gmail.com>
2020-09-24 20:11:52 +0530

    Updates

---
+++ precache-manifest.f5b11a3b481b20ba591c1b31b90e0c40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ceac74627764255648a2e0b5076b7ee",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "713dd0b8ce5ea5648444",
+    "url": "/first-react-app/static/css/main.caae2f52.chunk.css"
+  },
+  {
+    "revision": "9495adcf22c1c6c25f1e",
+    "url": "/first-react-app/static/js/2.7df42795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app/static/js/2.7df42795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713dd0b8ce5ea5648444",
+    "url": "/first-react-app/static/js/main.bf855c98.chunk.js"
+  },
+  {
+    "revision": "d53224d434734c0815ad",
+    "url": "/first-react-app/static/js/runtime-main.751e4890.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
